93e51451be19f8ba32671670fdd80bbdfd0bb0c5
Ильназ <alexsanderkyznecov@mail.ru>
2023-12-27 13:43:22 +0500

    классная работа(Class work)

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 25;
+int b = 5;
+
+if (b * b == a)
+  Console.WriteLine ("true");
+  else
+  Console.WriteLine ("false");
+// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго.
