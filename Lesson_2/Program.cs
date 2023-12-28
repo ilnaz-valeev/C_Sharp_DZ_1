bba545cf90ca3d4412c0abb19b7259492d2e63fe
Ильназ <alexsanderkyznecov@mail.ru>
2023-12-28 21:56:18 +0500

    3 ае(3)

---
+++ Lesson_2/Program.cs
@@ -0,0 +1,6 @@
+﻿// 1. Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+Console.WriteLine("Enter Number");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(a / 100 * 10 + a % 10);
